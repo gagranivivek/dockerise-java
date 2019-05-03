4a7537f3ec7df7b55d94299661b499787a3cac24
Sumanth <DevarakondaSumanth@JohnDeere.com>
2019-05-03 12:25:35 +0530

    first commit

---
+++ src/main/java/com/training/dockerisedjava/DockerisedJavaApplication.java
@@ -0,0 +1,13 @@
+package com.training.dockerisedjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerisedJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerisedJavaApplication.class, args);
+	}
+
+}
